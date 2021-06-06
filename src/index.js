1726ad756376746dde0e38399b34dcec33d0065f
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2021-06-07 00:00:14 +0200

    Reorder components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import { injectShell } from './components/injectShell';
-import { toggleModal } from './components/toggleModal';
 import { logMediaDevices } from './components/logMediaDevices';
+import { toggleModal } from './components/toggleModal';
+import {
+  toggleVideoPreview,
+  toggleVideoPlayback,
+} from './components/toggleStream';
 import { modalContent } from './components/modalContent';
 import { startStream } from './components/startStream';
 import { stopStream } from './components/stopStream';
 import { startRecorder } from './components/startRecorder';
 import { stopRecorder } from './components/stopRecorder';
 import { uploadVideo } from './components/uploadVideo';
-import {
-  toggleVideoPreview,
-  toggleVideoPlayback,
-} from './components/toggleStream';
 
 injectShell();
 logMediaDevices();
@@ -20,13 +20,14 @@ logMediaDevices();
 
 export {
   injectShell,
+  logMediaDevices,
   toggleModal,
   toggleVideoPreview,
   toggleVideoPlayback,
+  modalContent,
   startStream,
   stopStream,
   startRecorder,
   stopRecorder,
   uploadVideo,
-  modalContent,
 };
