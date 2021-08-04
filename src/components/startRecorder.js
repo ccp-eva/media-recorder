c911ce5b75428b4b93e498b91f95b09b9df9e142
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2021-08-05 00:13:00 +0200

    startRecorder is always creating a new stream object
    this elimantes timeouts

--- src/components/startRecorder.js
+++ src/components/startRecorder.js
@@ -1,5 +1,3 @@
-import { startStream } from './startStream';
-
 export const startRecorder = (
   constraintObject = {
     audio: true,
@@ -7,31 +5,44 @@ export const startRecorder = (
     frameRate: 15,
   },
 ) => {
-  // check if there is an active stream, if not start one
-  if (!('localStream' in window && window.localStream.active)) {
-    startStream(constraintObject);
-  }
-  // todo use a promise here instead of timeout
-  setTimeout(() => {
-    let options = { mimeType: 'video/webm;codecs=vp9,opus' };
+  let options = { mimeType: 'video/webm;codecs=vp9,opus' };
+  if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+    console.error(`${options.mimeType} is not supported, using vp8`);
+    options = { mimeType: 'video/webm;codecs=vp8,opus' };
     if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-      console.error(`${options.mimeType} is not supported, using vp8`);
-      options = { mimeType: 'video/webm;codecs=vp8,opus' };
+      console.error(`${options.mimeType} is not supported`);
+      options = { mimeType: 'video/webm' };
       if (!MediaRecorder.isTypeSupported(options.mimeType)) {
         console.error(`${options.mimeType} is not supported`);
-        options = { mimeType: 'video/webm' };
-        if (!MediaRecorder.isTypeSupported(options.mimeType)) {
-          console.error(`${options.mimeType} is not supported`);
-          options = { mimeType: '' };
-        }
+        options = { mimeType: '' };
       }
     }
-    // recrod stream
-    window.mediaRecorder = new MediaRecorder(window.localStream, options);
-    window.dataChunks = [];
-    window.mediaRecorder.start();
-    console.log(window.mediaRecorder.state);
-    window.mediaRecorder.ondataavailable = (e) =>
-      window.dataChunks.push(e.data);
-  }, 2000);
+  }
+
+  // create a new stream
+  navigator.mediaDevices
+    .getUserMedia(constraintObject)
+    .then((stream) => {
+      window.localStream = stream;
+      const video = document.querySelector('#video-preview');
+
+      if ('srcObject' in video) {
+        video.srcObject = stream;
+      } else {
+        // legacy version
+        video.src = window.URL.createObjectURL(stream);
+      }
+
+      // Attach stream
+      video.onloadedmetadata = () => video.play();
+
+      // recrod stream
+      window.mediaRecorder = new MediaRecorder(window.localStream, options);
+      window.dataChunks = [];
+      window.mediaRecorder.start();
+      console.log(window.mediaRecorder.state);
+      window.mediaRecorder.ondataavailable = (e) =>
+        window.dataChunks.push(e.data);
+    })
+    .catch((err) => console.log(err.name, err.message));
 };
